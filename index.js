c483cf62d6b828224d69082cff0da3d7111a345a
nao20010128nao <nao20010128@gmail.com>
2018-09-17 21:48:03 +0900

    hex使用ルートに変更

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 const app = express();
 
 app.get("/proxy/:url", (req, res) => {
-    const url = decodeURIComponent(req.params.url);
+    const url = Buffer.from(req.params.url, "hex").toString("utf8");
     const respond = ares => {
         res.status(ares.status).send(ares.data);
     };
